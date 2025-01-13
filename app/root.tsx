69e6183a1da6908c1259d20804ab0f9f27e837d1
Aaron Briceno <aaronbmejia@gmail.com>
2025-01-13 11:14:56 -0500

    Shopify Vercel Template title edits

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ([
   { charset: "utf-8" },
-  { title: "Courier Api Integration" },
+  { title: "Shopify Vercel Template" },
   { viewport: "width=device-width,initial-scale=1" },
 ]);
 
